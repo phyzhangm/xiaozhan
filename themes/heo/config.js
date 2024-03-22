ded0c5630585d9f72406a5939fdba6d96c9cd49f
phyzhangm <119054186+phyzhangm@users.noreply.github.com>
2024-03-22 18:34:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的小站', url: 'https://www.phyzhangm.top' },
-    { title: '希望你能在这里收获快乐', url: 'https://www.phyzhangm.top' }
+    { title: '🎉欢迎来到我的小站🎉', url: 'https://www.phyzhangm.top' },
+    { title: '🎉希望你能在这里收获快乐🎉', url: 'https://www.phyzhangm.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -25,7 +25,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/学习资料' },
+  HEO_HERO_CATEGORY_3: { title: '学习资料', url: '/tag/学习资料' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -36,10 +36,10 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '🤝 专修教研与教学',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
+    '🏠 物理教学小能手',
+    '🤖️ 知识分享爱好者',
     '🧱 团队小组发动机'
   ],
   HEO_INFO_CARD_URL: 'https://github.com/phyzhangm/', // 个人资料底部按钮链接
@@ -108,7 +108,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://www.phyzhangm.top/about',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
